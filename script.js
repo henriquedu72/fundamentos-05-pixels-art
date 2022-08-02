8198927b6c6d73e966c81f98a03503be1f197902
eduardo <duh_henrique@hotmail.com>
2022-08-02 20:01:40 -0300

    requisitos 4 e 5 finalizados

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ document.getElementById('black').style.border = 'solid black 1px';
 document.getElementById('aqua').style.border = 'solid black 1px';
 document.getElementById('blueviolet').style.border = 'solid black 1px';
 document.getElementById('pink').style.border = 'solid black 1px';
+
+
+// let pixelBoard = document.querySelector('#pixel-board');
+// for (let index = 0; index < 5; index += 1) {
+//     let pixelBoardDivs = pixelBoard[index];
+
+//     let pixelBoardElements = document.creatElement('div');
+//     pixelBoard.appendChild(pixelBoardElements);
+//     document.querySelector('#pixel-board').class  = "pixel";
+// }
+
+
