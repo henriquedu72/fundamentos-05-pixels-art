4fba0ddec65a9e3d84a432294a336eaa193ab414
eduardo <duh_henrique@hotmail.com>
2022-08-02 16:58:13 -0300

    requisito 2 e 3 finalizados

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('black').style.border = 'solid black 1px';
+document.getElementById('aqua').style.border = 'solid black 1px';
+document.getElementById('blueviolet').style.border = 'solid black 1px';
+document.getElementById('pink').style.border = 'solid black 1px';
