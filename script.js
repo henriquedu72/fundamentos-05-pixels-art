4f7204947fb387224a6a2fe3565ede3f8cb5abba
eduardo <duh_henrique@hotmail.com>
2022-08-04 01:21:31 -0300

    requisito 7 finalizado

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-document.getElementById('black').style.border = 'solid black 1px';
-document.getElementById('aqua').style.border = 'solid black 1px';
-document.getElementById('blueviolet').style.border = 'solid black 1px';
-document.getElementById('pink').style.border = 'solid black 1px';
+window.onload = function() {
+
+document.querySelector('.black').style.border = 'solid black 1px';
+document.querySelector('.aqua').style.border = 'solid black 1px';
+document.querySelector('.blueviolet').style.border = 'solid black 1px';
+document.querySelector('.pink').style.border = 'solid black 1px';
+document.querySelector('.pink').style.border = 'solid black 1px';
 
 
+
+// function criaDiv() {
 // let pixelBoard = document.querySelector('#pixel-board');
-// for (let index = 0; index < 5; index += 1) {
-//     let pixelBoardDivs = pixelBoard[index];
+// for (let index = 0; index < 25; index += 1) {
+//     let pixelBoardDivs = pixelBoardDivs[index];
 
 //     let pixelBoardElements = document.creatElement('div');
 //     pixelBoard.appendChild(pixelBoardElements);
 //     document.querySelector('#pixel-board').class  = "pixel";
 // }
+// criaDiv();
+// }
+
+function onClick() {
+    alert('hora da diversao');
+}
+onClick();
+
+function addSelected() {
+    let selected = document.querySelector('.black');
+    selected.classList.add("selected");
+}
+addSelected();
 
 
+// let colorPalette = document.querySelector('#color-palette');
+// function onClickColorcolorPalette(event) {
+//     alert('Clicou');
+//console.log(colorPalette); igual á:
+//console.log(event.target);
+// } foi pra dentro da proxima função :
 
-// document.getElementById('black').inner.class = "selected";
+function setColor() {
+    let selectedColorPalette = document.querySelector('#color-palette');
+    selectedColorPalette.addEventListener('click', function(event) {
+      let clickedPalette = event.target;
+      let getColor = document.querySelectorAll('.color');
+      getColor.forEach((element) => element.classList.remove('selected'));
+      clickedPalette.classList.add('selected');
+    });
+  }
+ 
+setColor();
+}
+
